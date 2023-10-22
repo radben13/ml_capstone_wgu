01a2569f8b03afb4725415ac3c4c2f100f21a1ec
Braden Van Wagenen <radben13@gmail.com>
2023-10-21 21:36:37 -0600

    fiddle with session state and buttons

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -5,19 +5,36 @@ import altair as alt
 
 
 st.title('Demo Streamlit')
-a = st.number_input('A', 0, 10, 1)
-b = st.number_input('B', 0, 10, 2)
+a = st.text_input('Label', 0, 10, 1)
+b = st.number_input('Value', 0, 10, 2)
 
-example_data = pd.DataFrame({'test': [a,b], 'val': ['a', 'b']})
-st.line_chart(example_data, x='val')
+def add_value():
+    st.session_state['custom_labels'] = np.array(st.session_state['custom_labels'].tolist() + [a])
+    st.session_state['custom_values'] = np.array(st.session_state['custom_values'].tolist() + [b])
+
+st.button('Add Values', on_click=add_value)
+
+if 'custom_values' not in st.session_state:
+    st.session_state['custom_labels'] = np.array(['value1','value2'])
+    st.session_state['custom_values'] = np.array([1,2], dtype=np.intp)
+
+
+example_data = pd.DataFrame({
+    'Labels': st.session_state['custom_labels'],
+    'Values': st.session_state['custom_values']
+})
+
+
+
+st.line_chart(example_data, x='Labels')
 # st.bar_chart(example_data, x='val')
 # st.area_chart(example_data, x='val')
 
 st.altair_chart(alt.Chart(example_data)    
-        .mark_line()
+        .mark_bar()
         .encode(
-            alt.X('val').title('value').axis(labelAngle=0),
-            alt.Y('test').title(None))
+            alt.X('Labels').axis(labelAngle=0),
+            alt.Y('Values').title(None))
     , use_container_width=True
 )
 
