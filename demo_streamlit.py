5c56dcad8ad5bb622b4f6970ea659335213be270
Braden Van Wagenen <radben13@gmail.com>
2023-10-21 21:02:25 -0600

    add static altair chart

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -1,15 +1,25 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
+
 
 st.title('Demo Streamlit')
 a = st.number_input('A', 0, 10, 1)
 b = st.number_input('B', 0, 10, 2)
 
-example_data = pd.DataFrame({'test': [a,b], 'val': ['a', 'b'], 'd': [2,4]})
+example_data = pd.DataFrame({'test': [a,b], 'val': ['a', 'b']})
 st.line_chart(example_data, x='val')
-st.bar_chart(example_data, x='val')
-st.area_chart(example_data, x='val')
+# st.bar_chart(example_data, x='val')
+# st.area_chart(example_data, x='val')
+
+st.altair_chart(alt.Chart(example_data)    
+        .mark_line()
+        .encode(
+            alt.X('val').title('value').axis(labelAngle=0),
+            alt.Y('test').title(None))
+    , use_container_width=True
+)
 
 st.write('<h1>hello streamlit app!</h1>', unsafe_allow_html=True)
 
