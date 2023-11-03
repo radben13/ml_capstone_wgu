9ad384a3aa67f20f5b02e035dafedd4573e2c8e3
Braden Van Wagenen <radben13@gmail.com>
2023-11-03 01:48:36 -0600

    add transformation start

--- resources/sql_scripts/data_cleanup.sql
+++ resources/sql_scripts/data_cleanup.sql
@@ -1,14 +1,5 @@
 
--- clean up the variable names to be consistent (remove the "_set_*" suffix)
--- saved as a view to only execute once at a later stage and save compute
-create or replace view clean_observations as
-select
-    stid as station_id
-    , date_time::timestamp_ntz as date_time
-    , regexp_substr(variable, $$(^.+)_set_[^_]+$$, 1,1,'c',1) variable
-    , value
-from observations
-;
+
 
 
 -- Create a combination table where all of the observations are available with a station at each point in time
--- src/Home.py
+++ src/Home.py
@@ -33,6 +33,7 @@ sections = {
     '3_demo': 'Application',
     '3_sample_json': 'Sample Timeseries',
     '3_transform_py': 'Parsing Script',
+    '3_upload': 'Snowflake Upload Script',
     '3_model_training': 'Training Data',
     '3_model_fitting': 'Fitting Details',
 
@@ -112,3 +113,52 @@ create_section_button('3_transform_py', sections)
 if st.session_state['3_transform_py']:
     "The function demonstrates how the object was parsed into a pandas DataFrame:"
     st.write(get_example_script_contents('python_snippets/transform_to_csv.py'))
+
+"""
+
+#### Snowflake
+
+The end goal of the retrieval was to get the data to Snowflake so the cleaning and preparation
+of data could be done with SQL and iterative transformation. Snowflake is a great tool
+for storing and transforming large amounts of structured data, and after pulling 10 days of
+observation data, I had over 2 Gigabytes of CSV files (when compressed in zip file).
+
+This totaled over two thousand CSV files, and I _was not_ going to upload that manually into
+Snowflake. To get the data loaded, I leveraged the `snowflake.snowpark` python package.
+When creating a `snowpark.DataFrame`, the module creates a temporary table in Snowflake
+that houses the data passed to it. Once there, it was a simple task to "write" the table
+in append mode to its destination.
+"""
+
+create_section_button('3_upload', sections)
+if st.session_state['3_upload']:
+    """
+    Using this script, I uploaded all of the data, three files at a time, into the
+    `ML_CAPSTONE.WEATHER.OBSERVATIONS` table in Snowflake (the database and schema were set in
+    the default configuration for the snowflake connection).
+    """
+    st.write(get_example_script_contents('python_snippets/load_observation_data.py'))
+
+"""
+Once in Snowflake, the work to analyze and transform the data for the task of training
+the model began.
+
+There were four transformations that needed to occur:
+1. Parse and label Weather Condition Codes as "High Risk" or not.
+2. Clean up variable names.
+3. Pivot and join observations to their stations.
+4. Remove stations lacking the material data required for the model.
+
+Step 1 is covered in the [descriptive section](/Descriptive).
+
+For step 2, the variable names were split into groups with different `_set` suffixes. Those needed
+to be removed for the training data to be consistent. To accomplish this, the following view was
+created on top of the observations table:
+
+"""
+
+st.write(get_example_script_contents('sql_scripts/clean_observations.sql'))
+
+"""
+
+"""
