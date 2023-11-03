6c17f9354e199446422c89402ffa3a9c2ae864da
Braden Van Wagenen <radben13@gmail.com>
2023-11-03 05:44:42 -0600

    add link to github repo on home and add content on the repo

--- src/Home.py
+++ src/Home.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+from src.util.file_tools import get_asset_path
+
+with open(get_asset_path('invertocat.svg.data')) as f:
+    invertocat_svg = f.read()
+
 """
 # Weather Severity Prediction
 by Braden Van Wagenen &mdash; Student ID: 001345438
@@ -16,7 +21,8 @@ organization with a [Weather API](https://synopticdata.com/weatherapi/) that ena
 of weather station observation data across the world. With the Open Access License, 1 year of historical
 data is available for academic institutions or hobbyists to retrieve free of charge.
 
-Feel free to navigate through this site to observe the capstone. The navigation is intentionally sorted in a
+Feel free to navigate through this site or the GitHub repo, [![GitHub Invertocat](""" + invertocat_svg + """)](https://github.com/radben13/ml_capstone_wgu)
+[radben13/ml_capstone_wgu](https://github.com/radben13/ml_capstone_wgu), to observe the capstone. The navigation is intentionally sorted in a
 top-to-bottom order for ease of consumption, but there is no requirement for going in order.
 
 """
